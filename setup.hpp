74b17932864fce75acd3fa5f90cf24cff072e70d
Ryan <rgpirie07@gmail.com>
2023-10-31 22:14:54 -0700

    Basic validation layers added for error checking.

--- setup.hpp
+++ setup.hpp
@@ -4,6 +4,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <vector>
+
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
@@ -15,10 +17,19 @@ public:
 private:
     GLFWwindow* window;
 
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+
     void initWindow();
     void initVulkan();
     void mainLoop();
     void cleanUp();
+    void createInstance();
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+    void setupDebugMessenger();
+    std::vector<const char *> getRequiredExtensions();
+    bool checkValidationLayerSupport();
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData);
 };
 
 #endif
